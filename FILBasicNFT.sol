2c21771ac300cb24c2a221a66f88afe104b0eeb3
Varun Doshi <61531351+varun-doshi@users.noreply.github.com>
2024-02-23 20:17:27 +0530

    Create FILBasicNFT.sol

---
+++ FILBasicNFT.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC721, Ownable {
+
+    
+
+ function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
